62f363e41f73d9452726a31db76aa1ae1c518928
Elmir Mikayilli <elmir.mikayilli@gmail.com>
2024-09-17 17:56:57 +0400

    Add project files.

---
+++ Entities/Concrete/ContactForm.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	public class ContactForm:BaseEntity
+	{
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public int UserId { get; set; }
+    }
+}
